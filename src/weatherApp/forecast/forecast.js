03b0e6016d6e0d7843120f6be3d111115be75f59
Moti Matatyahu <motke.mat@gmail.com>
2018-06-18 23:27:49 +0300

    add tempature support

---
+++ src/weatherApp/forecast/forecast.js
@@ -0,0 +1,33 @@
+const request = require('request');
+
+
+
+const forecastByCoordinates = (coordinates, callback) => {
+
+    request({
+        url: `https://api.darksky.net/forecast/297a4af865bdd23c2bdbf471252f9933/${coordinates.latitude},${coordinates.longitude}?units=si`,
+        json: true
+    }, (error, response, body) => {
+
+        if (error) {
+            callback("Unable to connect to Forecast.io-Services")
+        }
+        else {
+            switch (response.statusCode) {
+                case 400:
+                    callback(body.error);
+                    break;
+                case 200:
+                    callback(undefined, results = {
+                        address: coordinates.address,
+                        temperature: body.currently.temperature
+                    })
+                    break;
+            }
+        }
+    });
+}
+
+module.exports = {
+    forecastByCoordinates
+}
--- src/weatherApp/weatherApp.js
+++ src/weatherApp/weatherApp.js
@@ -1,12 +1,21 @@
 
 const argumentor = require('./weatherArgumentor');
 const goeocode = require('./goeocode/goeocode');
+const forecast = require('./forecast/forecast');
 
 var args = argumentor.argv;
 
 goeocode.goeocodeAddress(args.address, (error, results) => {
   if (error)
     console.log(error);
-  if (results)
-    console.log(JSON.stringify(results, undefined, 2));
+  if (results) { 
+    forecast.forecastByCoordinates(results, (weatherError, weatherResult) => {
+      if (weatherError)
+        console.log(weatherError);
+      if (weatherResult)
+        console.log(JSON.stringify(weatherResult, undefined, 2));
+    });
+  }
 });
+
+
