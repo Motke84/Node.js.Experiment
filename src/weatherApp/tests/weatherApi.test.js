228fdf0e03c4506bbe3851484c3f13d8ca034f19
Moti Matatyahu <motke.mat@gmail.com>
2018-07-05 09:45:40 +0300

    test cont

--- src/weatherApp/tests/weatherApi.test.js
+++ src/weatherApp/tests/weatherApi.test.js
@@ -1,21 +1,33 @@
 require('dotenv').config();
+const expect = require('expect');
 const weatherApi = require('../weatherApi/weatherApi');
 
 
-it('should return object with weather and address', () => {
-    var res = weatherApi.getWeather("Natan 12 Ramat-Gan").payload
+it('should return object with weather and address', (done) => {
+  var res = weatherApi.getWeather("Natan 12 Ramat-Gan").payload
     .then(data => {
-        if (!data.address || !data.weather) {
-            throw new Error(`Expected full object , but got ${data}.`)
-          }
-    });  
-  });
+      if (!data.address || !data.weather) {
+        throw new Error(`Expected full object , but got ${data}.`)
+      }
+      done();
+    });
+});
 
-  it('should return object with error', () => {
-    var res = weatherApi.getWeather("Martian 1112 Mars").payload
+it('should return object with error', (done) => {
+
+  var res = weatherApi.getWeather("Martian 1112 Mars").payload
     .then(data => {
-        if (!data.error) {
-            throw new Error(`Expected object with error, but got ${error}.`)
+
+      const json = JSON.stringify(data);
+      console.log('Data:',json);
+      
+      expect(data)
+        .toEqual({
+          error: {
+            error_message: weatherApi.error_messages.ZERO_RESULTS
           }
-    });  
-  });
+        });
+
+      done();
+    })
+});
