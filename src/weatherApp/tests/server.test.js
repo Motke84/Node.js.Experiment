d385a40ab07703e22282aab5e939365405fb5c0c
Moti Matatyahu <motke.mat@gmail.com>
2018-07-14 21:42:17 +0300

    add more tests

--- src/weatherApp/tests/server.test.js
+++ src/weatherApp/tests/server.test.js
@@ -4,30 +4,55 @@ const expect = require('expect');
 const weatherApi = require('../weatherApi/weatherApi');
 const app = require('../server').app;
 
-it('server.test - should return Page not found response', (done) => {
-  request(app)
-    .get('/PageNotFound')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toHaveProperty(
-        'error', 'Page not found.');
-    })
-    .end(done);
+//https://jestjs.io/docs/en/23.2/expect#tobedefined
+
+describe('server-test', () => {
+
+  describe('#common', () => {
+    it('should return Page not found response', (done) => {
+      request(app)
+        .get('/PageNotFound')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toHaveProperty(
+            'error', 'Page not found.');
+        })
+        .end(done);
+    });
+  });
+
+  describe('#weather-api', () => {
+    it('should return address not found response', (done) => {
+      request(app)
+        .get('/getWeather?address=Martian 1112 Mars')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toHaveProperty(['result', 'error'], {
+            error_message: weatherApi.error_messages.ZERO_RESULTS
+          });
+        })
+        .end(done);
+    }).timeout(3000);
+
+    it('should return address found response', (done) => {
+      const addressToCheck = 'Natan St 12, Ramat Gan';
+
+      request(app)
+        .get(`/getWeather?address=${addressToCheck}`)
+        .expect(200)
+        .expect((res) => {
+
+          expect(res.body).toHaveProperty(['result', 'address']);
+          expect(res.body.result.address)
+            .toEqual(expect.stringContaining(addressToCheck));
+
+          expect(res.body).toHaveProperty(['result', 'weather']);
+
+        })
+        .end(done);
+    }).timeout(3000);
+
+  });
+
 });
-
-
-it('server.test - should return address not found response', (done) => {
-
-  request(app)
-    .get('/getWeather?address=Martian 1112 Mars')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toHaveProperty('result', {
-        error: {
-          error_message: weatherApi.error_messages.ZERO_RESULTS
-        }
-      });
-    })
-    .end(done);
-}).timeout(3000);
 
--- src/weatherApp/tests/weatherApi.test.js
+++ src/weatherApp/tests/weatherApi.test.js
@@ -3,28 +3,29 @@ const expect = require('expect');
 const weatherApi = require('../weatherApi/weatherApi');
 
 
-it('weatherApi.test - should return object with weather and address', (done) => {
-  var res = weatherApi.getWeather("Natan 12 Ramat-Gan").payload
-    .then(data => {
+describe('weatherApi-tests', () => {
 
-      expect(data).toHaveProperty('address');
-      expect(data).toHaveProperty('weather');
+  it('should return object with weather and address', (done) => {
+    var res = weatherApi.getWeather("Natan 12 Ramat-Gan").payload
+      .then(data => {
 
-      //  if (!data.address || !data.weather) {
-      //    throw new Error(`Expected full object , but got ${data}.`)
-      //  }
-      done();
-    });
-});
+        expect(data).toHaveProperty('address');
+        expect(data).toHaveProperty('weather');
 
-it('weatherApi.test - should return object with error', (done) => {
-
-  var res = weatherApi.getWeather("Martian 1112 Mars").payload
-    .then(data => {
-      expect(data).toHaveProperty('error', {
-        error_message: weatherApi.error_messages.ZERO_RESULTS
+        done();
       });
+  });
 
-      done();
-    })
-}).timeout(3000);
+  it('should return object with error', (done) => {
+
+    var res = weatherApi.getWeather("Martian 1112 Mars").payload
+      .then(data => {
+        expect(data).toHaveProperty('error', {
+          error_message: weatherApi.error_messages.ZERO_RESULTS
+        });
+
+        done();
+      })
+  }).timeout(3000);
+
+});
